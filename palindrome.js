9292a22a8ae88b8f79166dcb569bba37c2290b54
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-10-22 14:36:11 -0400

    updated files to return true if a palindrome, false if not

--- palindrome.js
+++ palindrome.js
@@ -1,11 +1,16 @@
 // Reverses a string.
 function reverse(string) {
-    return Array.from("").reverse().join("");
-  }
-
-
-// Returns true for a palindrome, false otherwise.
-function palindrome(string) {
-    let processedContent = string.toLowerCase();
-    return processedContent === reverse(processedContent);
+  return Array.from(string).reverse().join("");
 }
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    let processedContent = this.content.toLowerCase();
+    return processedContent === reverse(processedContent);
+  }
+}
+
