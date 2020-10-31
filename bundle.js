06ac255e055df2e767403d0d44e768b1785f106d
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-10-30 22:08:58 -0400

    add phrase variable to main.js

--- bundle.js
+++ bundle.js
@@ -2,6 +2,7 @@
 let Phrase = require("psarge-palindrome");
 
 let string = prompt("Please enter a string for palindrome testing:");
+let phrase = new Phrase(string);
 
 if (phrase.palindrome()) {
     alert(`"${phrase.content}" is a palindrome!`);
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let Phrase = require("psarge-palindrome");
 
 let string = prompt("Please enter a string for palindrome testing:");
+let phrase = new Phrase(string);
 
 if (phrase.palindrome()) {
     alert(`"${phrase.content}" is a palindrome!`);
